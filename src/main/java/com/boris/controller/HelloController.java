177f55442468f1e8f0f1d44fdc0e2589e29c139b
liyong1 <1191256202@qq.com>
2023-05-16 20:17:50 +0800

    springboot 第一个项目

---
+++ src/main/java/com/boris/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.boris.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author liyong
+ */
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String handle01(){
+        return "Hello, Spring Boot 2!";
+    }
+}
