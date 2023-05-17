80a0d135915f140b908c62bf5577f5fa6f4df71f
liyong1 <1191256202@qq.com>
2023-05-17 10:56:51 +0800

    提交hello world应用案例

--- Demo1/src/main/java/com/boris/controller/HelloController.java
+++ Demo1/src/main/java/com/boris/controller/HelloController.java
@@ -1,6 +1,5 @@
 package com.boris.controller;
 
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @RequestMapping("/hello")
-    public String handle01(){
+    public String handle01() {
         return "Hello, Spring Boot 2!";
     }
 }
